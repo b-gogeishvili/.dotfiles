a0fed1a24491e89cbd76aecf8e9d82dd284a1db4
b-gogeishvili <gogeishvilib@gmail.com>
2025-05-19 01:53:35 +0400

    fix: minor changes

--- .scripts/dock/monitors.sh
+++ .scripts/dock/monitors.sh
@@ -9,11 +9,13 @@ if [ "$num_monitors" -eq 3 ]; then
     hyprctl keyword monitor "DP-4,2560x1440@100,0x0,1,bitdepth,10"
     hyprctl keyword monitor "DP-5,1920x1080@100,-1080x-360,1,transform,1,bitdepth,10"
 
-    hyprctl dispatch moveworkspacetomonitor 1 DP-5
-    hyprctl dispatch moveworkspacetomonitor 4 DP-5
-
-    hyprctl dispatch moveworkspacetomonitor 2 DP-4
+    hyprctl dispatch moveworkspacetomonitor 1 DP-4
     hyprctl dispatch moveworkspacetomonitor 3 DP-4
+    hyprctl dispatch moveworkspacetomonitor 5 DP-4
+
+    hyprctl dispatch moveworkspacetomonitor 2 DP-5
+    hyprctl dispatch moveworkspacetomonitor 4 DP-5
+    hyprctl dispatch moveworkspacetomonitor 6 DP-5
 else
     hyprctl keyword monitor "eDP-1,1920x1200@60,0x0,1,bitdepth,10"
 fi
